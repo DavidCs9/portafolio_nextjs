6fbf95ab732a9a8e6317454883d85fa5e39a77f8
DavidCs9 <davidcastrosi@hotmail.com>
2023-03-24 18:22:53 -0600

    fix responsive

--- src/app/componentes/experiencia/experiencia.jsx
+++ src/app/componentes/experiencia/experiencia.jsx
@@ -1,16 +1,16 @@
 export function Experiencia () {
   return (
-    <section className=' h-96 p-3'>
+    <section className=' h-96 p-3 lg:w-[800px] m-auto'>
       <h1 className=' text-3xl text-white text-center'>EXPERIENCIA</h1>
       <div className=' text-white pt-6 text-left'>
         <p className=' text-2xl text-center text-slate-400'>
           Interceramic
         </p>
-        <p className=' text-lg text-center text-cyan-200'>
+        <p className=' text-2xl text-center text-cyan-200'>
           Desarrollador de software
         </p>
-        <p className=' ml-8 text-md pt-3'>Responsabilidades</p>
-        <ul className=' list-disc text-sm w-9/12 mx-auto pt-1'>
+        <p className=' ml-8 text-md pt-4 lg:text-center lg:text-2xl'>Responsabilidades</p>
+        <ul className=' list-disc text-sm w-9/12 mx-auto pt-1 lg:text-xl lg:pt-2'>
           <li>Apoyo requerimientos en ORACLE Forms y Reports</li>
           <li>Apoyo en creacion y mantenimiento de servicios web</li>
           <li>Investigacion para mejoras en procesos enfocados en TI</li>
