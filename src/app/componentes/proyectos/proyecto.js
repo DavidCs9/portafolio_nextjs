03308fd7cf9de49d66c512589a3ec33c746461f0
David Castro <davidcastrosi@hotmail.com>
2023-06-17 14:04:42 -0600

    add airtist project

--- src/app/componentes/proyectos/proyecto.js
+++ src/app/componentes/proyectos/proyecto.js
@@ -1,58 +1,67 @@
-export const proyecto = [
-  {
-    id: 1,
-    nombre: 'CastroBot',
-    descripcion: 'CastroBot es una aplicacion web que me permite consultar un asistente virtual usando la api de chatgpt.',
-    url: 'https://castrobot.tech/',
-    git: 'https://github.com/DavidCs9/castro-bot',
-    img: '/castrobot.webp',
-    tecnologias: ['nextjs', 'tailwindcss', 'chatgpt']
-  },
-  {
-    id: 2,
-    nombre: 'SluggerBet',
-    descripcion: 'Aplicacion web que muestra estadisticas de baseball y una IA para explicar los terminos.',
-    url: 'https://sluggerbet.fun/',
-    git: 'https://github.com/DavidCs9/sluggerbet',
-    img: '/hero-foto.webp',
-    tecnologias: ['nextjs', 'tailwind', 'mongodb']
-  },
-  {
-    id: 3,
-    nombre: 'Spotify Clone',
-    descripcion: 'Clone de la pagina de spotify. Proyecto para vacante front end developer en PID Electronics.',
-    url: 'https://spotify-clone-two-tau.vercel.app/',
-    git: 'https://github.com/DavidCs9/spotify-clone',
-    img: '/spotify.jpg',
-    tecnologias: ['vite', 'react', 'tailwindcss']
-  },
-  {
-    id: 4,
-    nombre: 'Marcos Dacar',
-    descripcion: 'Landing page para la empresa de enmarcado a la medida Marcos Dacar. Usando Astro y Tailwindcss.',
-    url: 'https://marcosdacar.com/',
-    git: 'https://github.com/DavidCs9/dacarWebsite',
-    img: '/dacar.webp',
-    tecnologias: ['astro', 'tailwindcss']
+export const proyecto = [{
+  id: 19219,
+  nombre: 'AIrtist',
+  descripcion: 'Aplicacion web para crear imagenes y compartirlas con la comunidad usando la api de DALLE.',
+  url: 'https://www.airtist.site/',
+  git: 'https://github.com/DavidCs9/AIrtist',
+  img: '/airtist.jpg',
+  tecnologias: ['react', 'nodejs', 'tailwindcss']
+},
 
-  },
-  {
-    id: 5,
-    nombre: 'Libreria Flask',
-    descripcion: 'Libreria con base de datos mysql, python flask en el backend, estilos con bootstrap.',
-    url: 'https://github.com/DavidCs9/Library',
-    git: 'https://github.com/DavidCs9/Library',
-    img: '/book4X3.jpg',
-    tecnologias: ['flask', 'bootstrap', 'mysql']
-  },
-  {
-    id: 6,
-    nombre: 'Movie Mind',
-    descripcion: 'Aplicacion web que recomienda peliculas basandose en el mood proporcionado por el usuario.',
-    url: 'https://movie-mind-1k382cnam-davidcs9.vercel.app/',
-    git: 'https://github.com/DavidCs9/movie-mind',
-    img: '/moviemind4X3.jpg',
-    tecnologias: ['astro', 'tailwind', 'svelte']
-  }
+{
+  id: 1,
+  nombre: 'CastroBot',
+  descripcion: 'CastroBot es una aplicacion web que me permite consultar un asistente virtual usando la api de chatgpt.',
+  url: 'https://castrobot.tech/',
+  git: 'https://github.com/DavidCs9/castro-bot',
+  img: '/castrobot.webp',
+  tecnologias: ['nextjs', 'tailwindcss', 'chatgpt']
+},
+{
+  id: 2,
+  nombre: 'SluggerBet',
+  descripcion: 'Aplicacion web que muestra estadisticas de baseball y una IA para explicar los terminos.',
+  url: 'https://sluggerbet.fun/',
+  git: 'https://github.com/DavidCs9/sluggerbet',
+  img: '/hero-foto.webp',
+  tecnologias: ['nextjs', 'tailwind', 'mongodb']
+},
+{
+  id: 3,
+  nombre: 'Spotify Clone',
+  descripcion: 'Clone de la pagina de spotify. Proyecto para vacante front end developer en PID Electronics.',
+  url: 'https://spotify-clone-two-tau.vercel.app/',
+  git: 'https://github.com/DavidCs9/spotify-clone',
+  img: '/spotify.jpg',
+  tecnologias: ['vite', 'react', 'tailwindcss']
+},
+{
+  id: 4,
+  nombre: 'Marcos Dacar',
+  descripcion: 'Landing page para la empresa de enmarcado a la medida Marcos Dacar. Usando Astro y Tailwindcss.',
+  url: 'https://marcosdacar.com/',
+  git: 'https://github.com/DavidCs9/dacarWebsite',
+  img: '/dacar.webp',
+  tecnologias: ['astro', 'tailwindcss']
+
+},
+{
+  id: 5,
+  nombre: 'Libreria Flask',
+  descripcion: 'Libreria con base de datos mysql, python flask en el backend, estilos con bootstrap.',
+  url: 'https://github.com/DavidCs9/Library',
+  git: 'https://github.com/DavidCs9/Library',
+  img: '/book4X3.jpg',
+  tecnologias: ['flask', 'bootstrap', 'mysql']
+},
+{
+  id: 6,
+  nombre: 'Movie Mind',
+  descripcion: 'Aplicacion web que recomienda peliculas basandose en el mood proporcionado por el usuario.',
+  url: 'https://movie-mind-1k382cnam-davidcs9.vercel.app/',
+  git: 'https://github.com/DavidCs9/movie-mind',
+  img: '/moviemind4X3.jpg',
+  tecnologias: ['astro', 'tailwind', 'svelte']
+}
 
 ]
